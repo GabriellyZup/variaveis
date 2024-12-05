d5eb97eb758b700b4cbbfdbfe16ab5d9d7d04b04
Gabrielly Boareto <gabrielly.boareto@zup.com.br>
2024-12-05 17:20:30 -0300

    array index= vai crescendo até o fim for while - passou

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,17 @@ public class Main {
     public static void main(String[] args) {
         int [] array = {1, 2, 3, 4, 5};
 
+    //exercício 1: Laço for
         for (int index = 0; index < array.length; index++) {
             System.out.println(array[index]);
         }
+
+    //exercício 2: laço while
+        int index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+
     }
 }
