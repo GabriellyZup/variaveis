8ddbddc90ff3324d164bf0acbc9411fa9b629ffd
GabriellyZup <gabrielly.boareto@zup.com.br>
2024-12-05 22:45:02 -0300

    Espero que tenha vindo o código
    os commits estavam se sobrepondo e quando criei o readme no intelij ele comecou a dar erro para subir o codigo.

---
+++ Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int [] array = {1, 2, 3, 4, 5};
+
+    //exercício 1: Laço for
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+
+    //exercício 2: laço while
+        int index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+
+    //exercício 3: Laço do-while
+        index = 0;
+        do {
+            System.out.println(array[index]);
+            index++;
+
+        } while (index < array.length);
+
+    }
+}
+//end
